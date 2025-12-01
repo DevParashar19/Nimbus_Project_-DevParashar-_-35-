fa303ea87a9be8c3edaae61780dd7cf0e3738304
MRINAL JHA <mrinaljha39@gmail.com>
2025-12-01 22:48:29 +0530

    Implement candidate management functions

--- candidate_module.c
+++ candidate_module.c
@@ -1,1 +1,30 @@
-h
+#include "project.h"
+
+Candidate candidates[50];
+int candidateCount = 0;
+
+void addCandidate() {
+    printf("\nEnter Candidate ID: ");
+    scanf("%d", &candidates[candidateCount].candidateID);
+
+    printf("Enter Candidate Name: ");
+    scanf("%s", candidates[candidateCount].candidateName);
+
+    candidates[candidateCount].voteCount = 0;
+
+    candidateCount++;
+    printf("Candidate added successfully!\n");
+}
+
+void viewCandidates() {
+    printf("\n---- Candidate List ----\n");
+
+    for (int i = 0; i < candidateCount; i++) {
+        printf("ID: %d | Name: %s | Votes: %d\n",
+            candidates[i].candidateID,
+            candidates[i].candidateName,
+            candidates[i].voteCount
+        );
+    }
+}
+
